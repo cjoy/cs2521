60ddb220d23d2d192757e2e61dcea9aaa4a8fee8
Chris Joy <chrisjoydev@gmail.com>
2018-12-02 20:45:46 +1100

    Incremental progress on ass1

--- ass1/test_textbuffer.c
+++ ass1/test_textbuffer.c
@@ -1,15 +1,28 @@
-////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 // COMP2521 19T0 ... assignment 1: Textbuffer ADT
 //
-// yyyy-mm-dd	Your Name Here <z5555555@unsw.edu.au>
+// 2018-12-14 Chris Joy <z5113243@unsw.edu.au>
 
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "textbuffer.h"
 
+// void parse (const char *text);
+// void parse (const char *text)
+// {
+// 	char *mutable_text, *line;
+// 	mutable_text = strdup (text);
+// 	while ((line = strsep (&mutable_text, "\n"))) {
+// 		printf("line: %s\n", line);
+// 	}
+// }
+
 int main (void)
 {
-	// your tests go here!
+	const char *text = "hello world \n this was a \n\n triumph \n ending the textfile now \0 sneaky";
+	Textbuffer tb = textbuffer_new(text);
+	// printf("last line: %s", tb->line);
 	return EXIT_SUCCESS;
 }
--- ass1/textbuffer.c
+++ ass1/textbuffer.c
@@ -1,8 +1,35 @@
 ////////////////////////////////////////////////////////////////////////
 // COMP2521 19T0 ... assignment 1: Textbuffer ADT
 //
-// yyyy-mm-dd	Your Name Here <z5555555@unsw.edu.au>
+// 2018-12-14 Chris Joy <z5113243@unsw.edu.au>
 
 #include "textbuffer.h"
 
-// your implementation goes here.
+typedef struct textbuffer *Textbuffer;
+typedef struct textbuffer {
+  char *line;
+  Textbuffer prev;
+  Textbuffer next;
+} textbuffer;
+
+// Example text:
+// hello world \n this was a triumph \n ending the textfile now \0
+Textbuffer textbuffer_new (const char *text)
+{
+	// char *mutable_text, *line;
+	// mutable_text = strdup(text);
+	// Textbuffer head = NULL;
+  // while ((line = strsep(&mutable_text, "\n"))) {
+  //   Textbuffer node = malloc(sizeof *node);
+  //   node->line = "Test";
+  //   node->next = NULL;
+  //   node->prev = NULL;
+  //   head = node;
+	// }
+  // return head;
+  Textbuffer node = malloc(sizeof (*node));
+  node->line = "Test";
+  node->next = NULL;
+  node->prev = NULL;
+  return node;
+}
