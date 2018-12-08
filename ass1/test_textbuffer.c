842d65f992758fce26b456c4fe33b0a1b6a485de
Chris Joy <chrisjoydev@gmail.com>
2018-12-08 14:11:16 +1100

    linted c code manually

--- ass1/test_textbuffer.c
+++ ass1/test_textbuffer.c
@@ -39,293 +39,293 @@ int main (void)
 	test_tb_search ();
 	// test_tb_replace ();
 
-	puts("All tests passed. You are awesome!");
+	puts ("All tests passed. You are awesome!");
 	return EXIT_SUCCESS;
 }
 
 void test_tb_new (void)
 {
-	puts("textbuffer_new():");
+	puts ("textbuffer_new ():");
 	{
-		puts("- testing empty string");
-		Textbuffer tb = textbuffer_new("");
-		assert(textbuffer_lines(tb) == 1);
-		textbuffer_drop(tb);
+		puts ("- testing empty string");
+		Textbuffer tb = textbuffer_new ("");
+		assert (textbuffer_lines (tb) == 1);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing consecutive new lines");
-		Textbuffer tb = textbuffer_new("\n\n\n");
-		assert(textbuffer_lines(tb) == 3);
-		textbuffer_drop(tb);
+		puts ("- testing consecutive new lines");
+		Textbuffer tb = textbuffer_new ("\n\n\n");
+		assert (textbuffer_lines (tb) == 3);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing string with consecutive new lines");
-		Textbuffer tb = textbuffer_new("hello world\nanother one\n\nblah\n");
-		assert(textbuffer_lines(tb) == 4);
-		textbuffer_drop(tb);
+		puts ("- testing string with consecutive new lines");
+		Textbuffer tb = textbuffer_new ("hello world\nanother one\n\nblah\n");
+		assert (textbuffer_lines (tb) == 4);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing string that terminates early");
-		Textbuffer tb = textbuffer_new("this is cool\nblah blah\n\0\nblah\n");
-		assert(textbuffer_lines(tb) == 2);
-		textbuffer_drop(tb);
+		puts ("- testing string that terminates early");
+		Textbuffer tb = textbuffer_new ("this is cool\nblah blah\n\0\nblah\n");
+		assert (textbuffer_lines (tb) == 2);
+		textbuffer_drop (tb);
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_bytes (void)
 {
-	puts("textbuffer_bytes():");
+	puts ("textbuffer_bytes ():");
 	{
-		puts("- testing ");
+		puts ("- testing ");
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_to_str (void)
 {
-	puts("textbuffer_to_str():");
+	puts ("textbuffer_to_str ():");
 	{
-		puts("- testing an empty textbuffer");
-		Textbuffer tb = textbuffer_new("");
-		assert(strcmp(textbuffer_to_str(tb), "\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing an empty textbuffer");
+		Textbuffer tb = textbuffer_new ("");
+		assert (strcmp (textbuffer_to_str (tb), "\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing one line textbuffer");
+		puts ("- testing one line textbuffer");
 		const char* str = "this is a one line textbuffer\n";
-		Textbuffer tb = textbuffer_new(str);
-		assert(strcmp(textbuffer_to_str(tb), str) == 0);
-		textbuffer_drop(tb);
+		Textbuffer tb = textbuffer_new (str);
+		assert (strcmp (textbuffer_to_str (tb), str) == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing a textbuffer with consecutive new lines");
+		puts ("- testing a textbuffer with consecutive new lines");
 		const char* str = "\n\n\n435\n3234\n";
-		Textbuffer tb = textbuffer_new(str);
-		assert(strcmp(textbuffer_to_str(tb), str) == 0);
-		textbuffer_drop(tb);
+		Textbuffer tb = textbuffer_new (str);
+		assert (strcmp (textbuffer_to_str (tb), str) == 0);
+		textbuffer_drop (tb);
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_swap (void)
 {
-	puts("textbuffer_swap():");
+	puts ("textbuffer_swap ():");
 	{
-		puts("- testing case: H <-[]-> <-[pos1]-> <-[]-> <-[pos3]-> <-[]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 1, 3);
-		assert(strcmp(textbuffer_to_str(tb), "0\n3\n2\n1\n4\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[]-> <-[pos1]-> <-[]-> <-[pos3]-> <-[]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 1, 3);
+		assert (strcmp (textbuffer_to_str (tb), "0\n3\n2\n1\n4\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[pos0]-> <-[]-> <-[]-> <-[]-> <-[pos4]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 0, 4);
-		assert(strcmp(textbuffer_to_str(tb), "4\n1\n2\n3\n0\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[pos0]-> <-[]-> <-[]-> <-[]-> <-[pos4]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 0, 4);
+		assert (strcmp (textbuffer_to_str (tb), "4\n1\n2\n3\n0\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[pos0]-> <-[]-> <-[]-> <-[pos3]-> <-[]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 0, 3);
-		assert(strcmp(textbuffer_to_str(tb), "3\n1\n2\n0\n4\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[pos0]-> <-[]-> <-[]-> <-[pos3]-> <-[]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 0, 3);
+		assert (strcmp (textbuffer_to_str (tb), "3\n1\n2\n0\n4\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[]-> <-[pos1]-> <-[]-> <-[]-> <-[pos4]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 1, 4);
-		assert(strcmp(textbuffer_to_str(tb), "0\n4\n2\n3\n1\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[]-> <-[pos1]-> <-[]-> <-[]-> <-[pos4]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 1, 4);
+		assert (strcmp (textbuffer_to_str (tb), "0\n4\n2\n3\n1\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[]-> <-[pos1]-> <-[pos2]-> <-[]-> <-[]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 1, 2);
-		assert(strcmp(textbuffer_to_str(tb), "0\n2\n1\n3\n4\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[]-> <-[pos1]-> <-[pos2]-> <-[]-> <-[]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 1, 2);
+		assert (strcmp (textbuffer_to_str (tb), "0\n2\n1\n3\n4\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[pos0]-> <-[pos1]-> <-[]-> <-[]-> <-[]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 0, 1);
-		assert(strcmp(textbuffer_to_str(tb), "1\n0\n2\n3\n4\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[pos0]-> <-[pos1]-> <-[]-> <-[]-> <-[]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 0, 1);
+		assert (strcmp (textbuffer_to_str (tb), "1\n0\n2\n3\n4\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[]-> <-[]-> <-[]-> <-[pos3]-> <-[pos4]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 3, 4);
-		assert(strcmp(textbuffer_to_str(tb), "0\n1\n2\n4\n3\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[]-> <-[]-> <-[]-> <-[pos3]-> <-[pos4]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 3, 4);
+		assert (strcmp (textbuffer_to_str (tb), "0\n1\n2\n4\n3\n") == 0);
+		textbuffer_drop (tb);
 	}
 	{
-		puts("- testing case: H <-[]-> <-[]-> <-[]-> <-[pos3|pos3]-> <-[]-> T");
-		Textbuffer tb = textbuffer_new(TEST_STR_1);
-		textbuffer_swap(tb, 3, 3);
-		assert(strcmp(textbuffer_to_str(tb), "0\n1\n2\n3\n4\n") == 0);
-		textbuffer_drop(tb);
+		puts ("- testing case: H <-[]-> <-[]-> <-[]-> <-[pos3|pos3]-> <-[]-> T");
+		Textbuffer tb = textbuffer_new (TEST_STR_1);
+		textbuffer_swap (tb, 3, 3);
+		assert (strcmp (textbuffer_to_str (tb), "0\n1\n2\n3\n4\n") == 0);
+		textbuffer_drop (tb);
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_insert (void)
 {
-	puts("textbuffer_insert():");
+	puts ("textbuffer_insert ():");
 	{
-		puts("- testing front insertion");
-		Textbuffer tb1 = textbuffer_new("a+\nb+\nc+\n");
-		Textbuffer tb2 = textbuffer_new("a-\nb-\nc-\n");
+		puts ("- testing front insertion");
+		Textbuffer tb1 = textbuffer_new ("a+\nb+\nc+\n");
+		Textbuffer tb2 = textbuffer_new ("a-\nb-\nc-\n");
 		textbuffer_insert (tb1, 0, tb2);
-		assert(textbuffer_lines(tb1) == 6);
-		assert(strcmp(textbuffer_to_str(tb1), "a-\nb-\nc-\na+\nb+\nc+\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 6);
+		assert (strcmp (textbuffer_to_str (tb1), "a-\nb-\nc-\na+\nb+\nc+\n") == 0);
+		textbuffer_drop (tb1);
 	}
 	{
-		puts("- testing back insertion");
-		Textbuffer tb1 = textbuffer_new("a+\nb+\nc+\n");
-		Textbuffer tb2 = textbuffer_new("a-\nb-\nc-\n");
+		puts ("- testing back insertion");
+		Textbuffer tb1 = textbuffer_new ("a+\nb+\nc+\n");
+		Textbuffer tb2 = textbuffer_new ("a-\nb-\nc-\n");
 		textbuffer_insert (tb1, 3, tb2);
-		assert(textbuffer_lines(tb1) == 6);
-		assert(strcmp(textbuffer_to_str(tb1), "a+\nb+\nc+\na-\nb-\nc-\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 6);
+		assert (strcmp (textbuffer_to_str (tb1), "a+\nb+\nc+\na-\nb-\nc-\n") == 0);
+		textbuffer_drop (tb1);
 	}
 	{
-		puts("- testing somewhere in the middle");
-		Textbuffer tb1 = textbuffer_new("a+\nb+\nc+\n");
-		Textbuffer tb2 = textbuffer_new("a-\nb-\nc-\n");
+		puts ("- testing somewhere in the middle");
+		Textbuffer tb1 = textbuffer_new ("a+\nb+\nc+\n");
+		Textbuffer tb2 = textbuffer_new ("a-\nb-\nc-\n");
 		textbuffer_insert (tb1, 2, tb2);
-		assert(textbuffer_lines(tb1) == 6);
-		assert(strcmp(textbuffer_to_str(tb1), "a+\nb+\na-\nb-\nc-\nc+\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 6);
+		assert (strcmp (textbuffer_to_str (tb1), "a+\nb+\na-\nb-\nc-\nc+\n") == 0);
+		textbuffer_drop (tb1);
 	}
 	{
-		puts("- testing somewhere in the middle");
-		Textbuffer tb1 = textbuffer_new("a+\nb+\nc+\n");
-		Textbuffer tb2 = textbuffer_new("a-\nb-\nc-\n");
+		puts ("- testing somewhere in the middle");
+		Textbuffer tb1 = textbuffer_new ("a+\nb+\nc+\n");
+		Textbuffer tb2 = textbuffer_new ("a-\nb-\nc-\n");
 		textbuffer_insert (tb1, 1, tb2);
-		assert(textbuffer_lines(tb1) == 6);
-		assert(strcmp(textbuffer_to_str(tb1), "a+\na-\nb-\nc-\nb+\nc+\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 6);
+		assert (strcmp (textbuffer_to_str (tb1), "a+\na-\nb-\nc-\nb+\nc+\n") == 0);
+		textbuffer_drop (tb1);
 	}
 	{
-		puts("- testing insert single line tb2 into multiline tb1 middle");
-		Textbuffer tb1 = textbuffer_new("a\nb\nc\n");
-		Textbuffer tb2 = textbuffer_new("yep\n");
+		puts ("- testing insert single line tb2 into multiline tb1 middle");
+		Textbuffer tb1 = textbuffer_new ("a\nb\nc\n");
+		Textbuffer tb2 = textbuffer_new ("yep\n");
 		textbuffer_insert (tb1, 2, tb2);
-		assert(textbuffer_lines(tb1) == 4);
-		assert(strcmp(textbuffer_to_str(tb1), "a\nb\nyep\nc\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 4);
+		assert (strcmp (textbuffer_to_str (tb1), "a\nb\nyep\nc\n") == 0);
+		textbuffer_drop (tb1);
 	}	
 	{
-		puts("- testing insert single line tb2 into multiline tb1 start");
-		Textbuffer tb1 = textbuffer_new("a\nb\nc\n");
-		Textbuffer tb2 = textbuffer_new("yep\n");
+		puts ("- testing insert single line tb2 into multiline tb1 start");
+		Textbuffer tb1 = textbuffer_new ("a\nb\nc\n");
+		Textbuffer tb2 = textbuffer_new ("yep\n");
 		textbuffer_insert (tb1, 0, tb2);
-		assert(textbuffer_lines(tb1) == 4);
-		assert(strcmp(textbuffer_to_str(tb1), "yep\na\nb\nc\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 4);
+		assert (strcmp (textbuffer_to_str (tb1), "yep\na\nb\nc\n") == 0);
+		textbuffer_drop (tb1);
 	}	
 	{
-		puts("- testing insert single line tb2 into multiline tb1 end");
-		Textbuffer tb1 = textbuffer_new("a\nb\nc\n");
-		Textbuffer tb2 = textbuffer_new("yep\n");
+		puts ("- testing insert single line tb2 into multiline tb1 end");
+		Textbuffer tb1 = textbuffer_new ("a\nb\nc\n");
+		Textbuffer tb2 = textbuffer_new ("yep\n");
 		textbuffer_insert (tb1, 3, tb2);
-		assert(textbuffer_lines(tb1) == 4);
-		assert(strcmp(textbuffer_to_str(tb1), "a\nb\nc\nyep\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 4);
+		assert (strcmp (textbuffer_to_str (tb1), "a\nb\nc\nyep\n") == 0);
+		textbuffer_drop (tb1);
 	}	
 	{
-		puts("- testing insert mutliline line tb2 into single tb1 start");
-		Textbuffer tb1 = textbuffer_new("yep\n");
-		Textbuffer tb2 = textbuffer_new("a\nb\nc\n");
+		puts ("- testing insert mutliline line tb2 into single tb1 start");
+		Textbuffer tb1 = textbuffer_new ("yep\n");
+		Textbuffer tb2 = textbuffer_new ("a\nb\nc\n");
 		textbuffer_insert (tb1, 0, tb2);
-		assert(textbuffer_lines(tb1) == 4);
-		assert(strcmp(textbuffer_to_str(tb1), "a\nb\nc\nyep\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 4);
+		assert (strcmp (textbuffer_to_str (tb1), "a\nb\nc\nyep\n") == 0);
+		textbuffer_drop (tb1);
 	}
 	{
-		puts("- testing insert mutliline line tb2 into single tb1 end");
-		Textbuffer tb1 = textbuffer_new("yep\n");
-		Textbuffer tb2 = textbuffer_new("a\nb\nc\n");
+		puts ("- testing insert mutliline line tb2 into single tb1 end");
+		Textbuffer tb1 = textbuffer_new ("yep\n");
+		Textbuffer tb2 = textbuffer_new ("a\nb\nc\n");
 		textbuffer_insert (tb1, 1, tb2);
-		assert(textbuffer_lines(tb1) == 4);
-		assert(strcmp(textbuffer_to_str(tb1), "yep\na\nb\nc\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 4);
+		assert (strcmp (textbuffer_to_str (tb1), "yep\na\nb\nc\n") == 0);
+		textbuffer_drop (tb1);
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_paste (void)
 {
-	puts("textbuffer_paste():");
+	puts ("textbuffer_paste():");
 	{
-		puts("- testing duplicates tb2 and inserts somewhere in the middle");
-		Textbuffer tb1 = textbuffer_new("a+\nb+\nc+\n");
-		Textbuffer tb2 = textbuffer_new("a-\nb-\nc-\n");
+		puts ("- testing duplicates tb2 and inserts somewhere in the middle");
+		Textbuffer tb1 = textbuffer_new ("a+\nb+\nc+\n");
+		Textbuffer tb2 = textbuffer_new ("a-\nb-\nc-\n");
 		textbuffer_paste (tb1, 2, tb2);
-		assert(textbuffer_lines(tb1) == 6);
-		assert(strcmp(textbuffer_to_str(tb1), "a+\nb+\na-\nb-\nc-\nc+\n") == 0);
-		assert(textbuffer_lines(tb2) == 3);
-		assert(strcmp(textbuffer_to_str(tb2), "a-\nb-\nc-\n") == 0);
-		textbuffer_drop(tb1);
+		assert (textbuffer_lines (tb1) == 6);
+		assert (strcmp (textbuffer_to_str (tb1), "a+\nb+\na-\nb-\nc-\nc+\n") == 0);
+		assert (textbuffer_lines (tb2) == 3);
+		assert (strcmp (textbuffer_to_str (tb2), "a-\nb-\nc-\n") == 0);
+		textbuffer_drop (tb1);
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_cut (void)
 {
-	puts("textbuffer_cut():");
+	puts ("textbuffer_cut():");
 	{
-		puts("- testing ");
+		puts ("- testing ");
 	}
-	puts("");
+	puts ("");
 }
 
 void test_tb_copy (void)
 {
-	puts("textbuffer_copy():");
+	puts ("textbuffer_copy():");
 	{
-		puts("- testing ");
+		puts ("- testing ");
 	}
 }
 
 void test_tb_delete (void)
 {
-	puts("textbuffer_delete():");
+	puts ("textbuffer_delete():");
 	{
-		puts("- testing ");
+		puts ("- testing ");
 	}
 }
 
 void test_tb_search (void)
 {
-	puts("textbuffer_search():");
+	puts ("textbuffer_search():");
 	{
-		puts("- testing a simple string");
-		Textbuffer tb = textbuffer_new("the cat sat mat\nthe cat ate hat\ncat had a bat\n");
-		assert(textbuffer_search(tb, "cat", false) == 3);
-		assert(textbuffer_search(tb, "sat", false) == 1);
-		assert(textbuffer_search(tb, "the", false) == 2);
-		assert(textbuffer_search(tb, "banana", false) == -1);
+		puts ("- testing a simple string");
+		Textbuffer tb = textbuffer_new ("the cat sat mat\nthe cat ate hat\ncat had a bat\n");
+		assert (textbuffer_search(tb, "cat", false) == 3);
+		assert (textbuffer_search(tb, "sat", false) == 1);
+		assert (textbuffer_search(tb, "the", false) == 2);
+		assert (textbuffer_search(tb, "banana", false) == -1);
 	}
 	{
-		puts("- testing a simple string in reverse");
-		Textbuffer tb = textbuffer_new("the cat sat mat\nthe cat ate hat\ncat had a bat\n");
-		assert(textbuffer_search(tb, "cat", true) == 3);
-		assert(textbuffer_search(tb, "sat", true) == 1);
-		assert(textbuffer_search(tb, "the", true) == 2);
-		assert(textbuffer_search(tb, "banana", true) == -1);
+		puts ("- testing a simple string in reverse");
+		Textbuffer tb = textbuffer_new ("the cat sat mat\nthe cat ate hat\ncat had a bat\n");
+		assert (textbuffer_search(tb, "cat", true) == 3);
+		assert (textbuffer_search(tb, "sat", true) == 1);
+		assert (textbuffer_search(tb, "the", true) == 2);
+		assert (textbuffer_search(tb, "banana", true) == -1);
 	}
 }
 
 void test_tb_replace (void)
 {
-	puts("textbuffer_replace():");
+	puts ("textbuffer_replace():");
 	{
-		puts("- testing ");
+		puts ("- testing ");
 	}
-	puts("");
+	puts ("");
 }
 
--- ass1/textbuffer.c
+++ ass1/textbuffer.c
@@ -35,20 +35,20 @@ dlink dlink_lookup (dlink list, size_t index);
 // Task 1
 Textbuffer textbuffer_new (const char *text)
 {
-  Textbuffer tb = textbuffer_new_node(); /* init a new textbuffer */
+  Textbuffer tb = textbuffer_new_node (); /* init a new textbuffer */
   dlink prev = NULL; /* temp prev node, so we can set dlink->prev */
 	char *line, *text_dup, *to_free;
-  to_free = text_dup = strdup(text);
-  if (*text_dup && text_dup[strlen(text_dup)-1] == '\n')
-    text_dup[strlen(text_dup)-1] = '\0'; /* remove trailing new line */
-  for (; (line = strsep(&text_dup, "\n")); tb->size++) {
-    dlink node = dlink_new_node(line);  /* create  new dlink node */
+  to_free = text_dup = strdup (text);
+  if (*text_dup && text_dup[strlen (text_dup)-1] == '\n')
+    text_dup[strlen (text_dup)-1] = '\0'; /* remove trailing new line */
+  for (; (line = strsep (&text_dup, "\n")); tb->size++) {
+    dlink node = dlink_new_node (line);  /* create  new dlink node */
     if (!tb->head) tb->head = node;     /* set head as first node */
     if (prev) prev->next = node;        /* set pre node's next to the new node */
     node->prev = prev;                  /* set prev */
     prev = node;                        /* new node now becomes prev */
 	}
-  free(to_free);          /* free duplicate text */
+  free (to_free);          /* free duplicate text */
   tb->tail = prev;        /* set tail as the last created node */
   tb->cursor = tb->head;  /* by default, we set the cursor at the head */
   return tb;
@@ -58,8 +58,8 @@ Textbuffer textbuffer_new (const char *text)
 void textbuffer_drop (Textbuffer tb)
 {
   if (!tb) return;
-  dlink_drop(tb->head);
-  free(tb);
+  dlink_drop (tb->head);
+  free (tb);
 }
 
 // Task 3
@@ -73,7 +73,7 @@ size_t textbuffer_bytes (Textbuffer tb)
 {
   size_t bytes = 0;
   for (dlink curr = tb->head; curr; curr = curr->next)
-    bytes = bytes + strlen(curr->data) + 1;
+    bytes = bytes + strlen (curr->data) + 1;
   return bytes;
 }
 
@@ -84,11 +84,11 @@ char *textbuffer_to_str (Textbuffer tb)
   size_t buff_len = 0;
   size_t i = 0;
   for (dlink curr = tb->head; curr; curr = curr->next) {
-    buff_len = (buff ? strlen(buff) : 0) + strlen(curr->data) + 2;
-    buff = realloc(buff, buff_len);
-    if (i == 0) strcpy(buff, curr->data);
-    if (i > 0) strcat(buff, curr->data);
-    strcat(buff, "\n"); /* concatenate new line to end of string */
+    buff_len = (buff ? strlen (buff) : 0) + strlen (curr->data) + 2;
+    buff = realloc (buff, buff_len);
+    if (i == 0) strcpy (buff, curr->data);
+    if (i > 0) strcat (buff, curr->data);
+    strcat (buff, "\n"); /* concatenate new line to end of string */
     i++;
   }
   return buff;
@@ -98,7 +98,7 @@ char *textbuffer_to_str (Textbuffer tb)
 void textbuffer_swap (Textbuffer tb, size_t pos1, size_t pos2)
 {
   if (pos1 >= tb->size || pos2  >= tb->size) {
-    fprintf(stderr, "pos{1|2} out of range");
+    fprintf (stderr, "pos{1|2} out of range");
     abort();
   }
 
@@ -111,8 +111,8 @@ void textbuffer_swap (Textbuffer tb, size_t pos1, size_t pos2)
   }
 
   /* retrieve nodes */
-  dlink A = dlink_lookup(tb->head, p1);
-  dlink B = dlink_lookup(tb->head, p2);
+  dlink A = dlink_lookup (tb->head, p1);
+  dlink B = dlink_lookup (tb->head, p2);
 
   /* if nodes are the same do nothing */
   if (A == B) return;
@@ -148,11 +148,11 @@ void textbuffer_swap (Textbuffer tb, size_t pos1, size_t pos2)
 void textbuffer_insert (Textbuffer tb1, size_t pos, Textbuffer tb2)
 {
   if (!tb1 || !tb2) {
-    fprintf(stderr, "tb{1|2} doesn't exist");
+    fprintf (stderr, "tb{1|2} doesn't exist");
     abort();
   }
   if (tb1->size < pos) {
-    fprintf(stderr, "pos out of range");
+    fprintf (stderr, "pos out of range");
     abort();
   }
 
@@ -161,7 +161,7 @@ void textbuffer_insert (Textbuffer tb1, size_t pos, Textbuffer tb2)
   if (tb1->size == 0) { tb1 = tb2; return; } 
   if (tb2->size == 0) return;
 
-  dlink A_start = dlink_lookup(tb1->head, pos);
+  dlink A_start = dlink_lookup (tb1->head, pos);
   dlink A_end = tb1->tail;
   dlink B_start = tb2->head;
   dlink B_end = tb2->tail;
@@ -191,7 +191,7 @@ void textbuffer_insert (Textbuffer tb1, size_t pos, Textbuffer tb2)
 // Task 8
 void textbuffer_paste (Textbuffer tb1, size_t pos, Textbuffer tb2)
 {
-  textbuffer_insert (tb1, pos, textbuffer_new(textbuffer_to_str(tb2)));
+  textbuffer_insert (tb1, pos, textbuffer_new (textbuffer_to_str (tb2)));
 }
 
 // Task 9
@@ -210,7 +210,7 @@ ssize_t textbuffer_search (Textbuffer tb, char *match, bool rev)
 {
   ssize_t found = 0;
   for (dlink curr = (rev ? tb->tail : tb->head); curr; curr = (rev ? curr->prev : curr->next))
-    if (strstr(curr->data, match)) found++;
+    if (strstr (curr->data, match)) found++;
   return (found == 0 ? -1 : found);
 }
 
@@ -231,7 +231,7 @@ ssize_t textbuffer_search (Textbuffer tb, char *match, bool rev)
 // Initialise a new textbuffer
 Textbuffer textbuffer_new_node(void)
 {
-  Textbuffer tb = malloc(sizeof(*tb));
+  Textbuffer tb = malloc (sizeof (*tb));
   tb->head = NULL;
   tb->tail = NULL;
   tb->cursor = NULL;
@@ -242,11 +242,11 @@ Textbuffer textbuffer_new_node(void)
 // Initialise a new doubly-linked list node
 dlink dlink_new_node (char *data)
 {
-  dlink node = malloc(sizeof(*node));
+  dlink node = malloc (sizeof (*node));
   node->next = NULL;
   node->prev = NULL;
-  node->data = malloc(strlen(data) + 1);
-  strcpy(node->data, data);
+  node->data = malloc (strlen (data) + 1);
+  strcpy (node->data, data);
   return node;
 }
 
@@ -257,7 +257,7 @@ void dlink_drop (dlink list)
 	while (list) {
 		node = list;
 		list = list->next;
-		free(node);
+		free (node);
 	}
 }
 
@@ -278,13 +278,13 @@ dlink dlink_lookup (dlink list, size_t index)
  */
 void print_text (Textbuffer tb)
 {
-  printf("\n------------------------------------------------------------------\n");
-  printf("Size: %zu\n", tb->size);
-  printf("Forward: ");
+  printf ("\n------------------------------------------------------------------\n");
+  printf ("Size: %zu\n", tb->size);
+  printf ("Forward: ");
   for (dlink curr = tb->head; curr; curr = curr->next)
-    printf("[%s]", curr->data);
-  printf("\nBackward: ");
+    printf ("[%s]", curr->data);
+  printf ("\nBackward: ");
   for (dlink curr = tb->tail; curr; curr = curr->prev)
-    printf("[%s]", curr->data);
-  printf("\n------------------------------------------------------------------\n");
+    printf ("[%s]", curr->data);
+  printf ("\n------------------------------------------------------------------\n");
 }
